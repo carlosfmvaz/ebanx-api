4b3d325b8c05dd53005c4c04d8486a0307944e72
Carlos Vaz <carlosfelipe_2002@outlook.com>
2023-08-28 19:17:42 -0300

    implementing the deposit test

--- src/modules/account/AccountController.test.ts
+++ src/modules/account/AccountController.test.ts
@@ -7,16 +7,23 @@ describe('AccountController test suite', () => {
     let accountController: any;
     let responseMock: any;
 
-    beforeEach(() => {
+    beforeAll(async () => {
         const accountRepositoryJson = new AccountRepositoryJson();
         const accountService = new AccountService(accountRepositoryJson);
         accountController = new AccountController(accountService);
+
+        // reset state
+        await accountController.restoreInitialState({}, {});
 
         responseMock = {
             status: jest.fn(() => responseMock),
             json: jest.fn(() => responseMock),
         };
     });
+
+    afterAll(async () => {
+        await accountController.restoreInitialState({}, {});
+    })
 
     it('should create account with initial balance', async () => {
         const requestMock = {
@@ -32,6 +39,24 @@ describe('AccountController test suite', () => {
             destination: {
                 id: "100",
                 balance: 10
+            }
+        });
+    });
+
+    it('should deposit into existing account', async () => {
+        const requestMock = {
+            body: {
+                type: "deposit",
+                destination: "100",
+                amount: 10
+            }
+        }
+        await accountController.handleEvent(requestMock, responseMock);
+        expect(responseMock.status).toBeCalledWith(201);
+        expect(responseMock.json).toHaveBeenCalledWith({
+            destination: {
+                id: "100",
+                balance: 20
             }
         });
     });
